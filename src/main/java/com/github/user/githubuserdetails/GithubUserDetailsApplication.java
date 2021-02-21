6a0d2dac5cafa4ac35290e4c1bbbb995333f5c72
bartekqx <bartlomiej.sulima7@gmail.com>
2021-02-21 14:59:04 +0100

    initial commit

---
+++ src/main/java/com/github/user/githubuserdetails/GithubUserDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.github.user.githubuserdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUserDetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubUserDetailsApplication.class, args);
+    }
+
+}
