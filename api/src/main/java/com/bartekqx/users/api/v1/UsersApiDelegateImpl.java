dc6ad2193fd0064571d21b029503369afca38900
bartekqx <bartlomiej.sulima7@gmail.com>
2021-02-21 19:39:05 +0100

    Little swagger enhance
    Little swagger enhance

---
+++ api/src/main/java/com/bartekqx/users/api/v1/UsersApiDelegateImpl.java
@@ -0,0 +1,15 @@
+package com.bartekqx.users.api.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsersApiDelegateImpl implements UsersApiDelegate {
+
+    @Override
+    public ResponseEntity<Void> getGithubUserDetails(String login) {
+        return null;
+    }
+}
